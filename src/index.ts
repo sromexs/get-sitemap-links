59d640353fa079386f19708a47ecd5cbdbb112d1
Sadra Bahrami <48912836+sromexs@users.noreply.github.com>
2023-03-03 14:29:44 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,53 @@
 import axios from "axios";
 
+/**
+ * Options for GetSitemapLinks function
+ * @property {string} [filterIndexes] - String to filter sitemap links by. If provided, only sitemap links that contain the specified string will be returned.
+ * For example, if filterIndexes is set to "blog", only sitemap links containing the word "blog" in their URL will be included.
+ */
 type IOptions = {
   filterIndexes?: string;
 };
 
-const GetSitemapLinks = async (url: string, options: IOptions = {}) => {
+/**
+ * Fetches all links from a sitemap URL, including nested sitemaps.
+ * @param {string} url - URL of the sitemap to fetch links from.
+ * @param {IOptions} [options] - Optional configuration options for the function.
+ * @returns {Promise<string[]>} - A promise that resolves with an array of all links found in the sitemap(s).
+ */
+const GetSitemapLinks = async (url: string, options: IOptions = {}): Promise<string[]> => {
+  const regex = /(?<=<loc>)(.+?)(?=<\/loc>)/g;
+  let indexes: string[] = [];
+  let links: string[] = [];
+
   try {
-    const hostname = new URL(url).hostname.replace(".", "\\.");
-    const regex = new RegExp(`https?:\/\/${hostname}.+?(?=<\/loc>)`, "gmi");
-
     const { data } = await axios.get(url);
-
-    let indexes = [...new Set((data as string).match(regex) as string[])];
-
-    if (indexes.length === 0 || !indexes[0].endsWith(".xml")) {
-      return indexes;
-    }
-
-    const { filterIndexes } = options;
-
-    if (filterIndexes) {
-      indexes = indexes.filter((e) => e.includes(filterIndexes));
-    }
-
-    let links: string[] = [];
-
-    for (const index of indexes) {
-      try {
-        const indexData = (await axios.get(index)).data as string;
-        const array = indexData.match(regex) as string[] | null;
-        if (array) links.push(...array);
-      } catch (err) {}
-    }
-
-    return [...new Set(links)];
+    indexes = [...new Set((data as string).match(regex) as string[])];
   } catch (err) {
     return [];
   }
+
+  if (indexes.length === 0 || !indexes[0].endsWith(".xml")) {
+    return indexes;
+  }
+
+  const { filterIndexes } = options;
+
+  if (filterIndexes) {
+    indexes = indexes.filter((e) => e.includes(filterIndexes));
+  }
+
+  await Promise.allSettled(
+    indexes.map(async (index) => {
+      try {
+        const { data: indexData } = await axios.get(index);
+        const array = indexData.match(regex) as string[] | null;
+        if (array) links.push(...array);
+      } catch (err) {}
+    })
+  );
+
+  return [...new Set(links)];
 };
 
 export default GetSitemapLinks;
